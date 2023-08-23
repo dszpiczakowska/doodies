2090bb5eda7d832138bafa5af6f30e87a45ee6c2
Daria Szpiczakowska <zaleska.daria92@gmail.com>
2023-08-23 08:37:15 +0200

    Empty project

---
+++ src/main/java/com/dszpiczakowska/doodies/DoodiesApplication.java
@@ -0,0 +1,13 @@
+package com.dszpiczakowska.doodies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoodiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dszpiczakowska/doodies/DoodiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dszpiczakowska.doodies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoodiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
